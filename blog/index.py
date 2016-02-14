e8a05fe12bb226ea2ed1f9bd0dc03538670663c4
linaro <xjq314@gmail.com>
2016-02-14 17:48:21 +0900

    Add navi to index

--- blog/index.py
+++ blog/index.py
@@ -25,18 +25,26 @@ class Index:
         index_output = codecs.open(index_file, mode='w', encoding='utf-8')
         # Write head
         index_output.write('<head>\n')
-        index_output.write('<link rel="stylesheet" type="text/css" href=' + conf.public_url + conf.css_index + '/>\n')
-        index_output.write('<link rel="shortcut icon" href=' + conf.public_url + conf.icon + '>\n')
+        index_output.write('<link rel="stylesheet" type="text/css" href=' + conf.css_index + '/>\n')
+        index_output.write('<link rel="shortcut icon" href=' + conf.icon + '>\n')
         index_output.write('<title>' + conf.blog_name + '</title>\n')
 	index_output.write('</head>\n')
+	# Write navi
+	index_output.write('<ul>')
+  	index_output.write('<li><a href=' + conf.public_url + '>' + conf.blog_name  + '</a></li>')
+	index_output.write('<ul style="float:right;list-style-tpye:none;">')
+    	index_output.write('<li><a href="https://github.com/xjq">Projects</a></li>')
+    	index_output.write('<li><a href="2015_12_25_blog_architecture.html">About</a></li>')
+ 	index_output.write('</ul>')
+	index_output.write('</ul>')
         # Write h1
-        index_output.write('<h1>' + conf.blog_name + '</h1>\n')
+        #index_output.write('<h1>' + conf.blog_name + '</h1>\n')
         # Write article list
         for html_name in html_list:
             if (html_name != 'index.html') & (html_name.find('html') > 0):
 	    #  if re.match(r'[0-9][0-9][0-9][0-9]_[0-9][0-9]_[0-9][0-9].*', name) is not None:
 		myarticle = article.Article(html_name.replace('html','md'))
 		essay_name = myarticle.essay_name
-                index_output.write('<a href="' + conf.public_url + html_name + '">' + essay_name + '</a>\n')
+                index_output.write('<a href="' + html_name + '">' + essay_name + '</a>\n')
                 index_output.write('<br></br>\n')
         index_output.close()
